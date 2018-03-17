9d95d88bd1f4b70744f0449fa96f516bc3743421
lilykju <34831497+lilykju@users.noreply.github.com>
2018-03-17 13:11:43 +0900

    Update quadtree.java

--- KangNaeun/Algospot/DivideConquer/quadtree.java
+++ KangNaeun/Algospot/DivideConquer/quadtree.java
@@ -14,9 +14,10 @@ public class quadtree {
 		while(t-->0){
 			String s= in.nextLine();
 			StringBuffer sb= new StringBuffer(s);
+			//iterator 
 			System.out.println(reverse(sb,0));
 			
-			//iterator »ç¿ë
+			//iterator ì‚¬ìš©
 			List<Character> listS=new ArrayList<Character>();
 			for(int i=0;i<s.length();i++) {
 				listS.add(s.charAt(i));
@@ -28,7 +29,7 @@ public class quadtree {
 		in.close();
 	}
 	
-	//Iterator »ç¿ë ÇÔ¼ö
+	//Iterator ì‚¬ìš© í•¨ìˆ˜
 	static String reverseIt(Iterator<Character> iterator) {
 		char head = iterator.next();
 		if(head!='x') return Character.toString(head);
