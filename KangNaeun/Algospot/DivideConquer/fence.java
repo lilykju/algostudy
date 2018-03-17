02724d724105ab17c5225b7c2ab04d083fa66cc8
lilykju <34831497+lilykju@users.noreply.github.com>
2018-03-17 13:08:24 +0900

    0317

---
+++ KangNaeun/Algospot/DivideConquer/fence.java
@@ -0,0 +1,45 @@
+package DivideConquer;
+
+import java.util.Scanner;
+
+public class fence {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while(t-->0){
+			int n = in.nextInt();
+			int[] f=new int[n];
+			for(int i=0;i<n;i++) {
+				f[i]= in.nextInt();
+			}
+			System.out.println(solve(f,0,f.length-1));
+			
+		}
+		in.close();
+	}
+	
+	static int solve(int[] f,int left,int right) {
+		//기저: 판자 1개
+		if(left==right) return f[left];
+		
+		int mid = (left+right)/2;
+		
+		//분할
+		int answer = Math.max(solve(f,left,mid), solve(f,mid+1,right));
+		
+		int lo=mid;
+		int hi=mid+1;
+		int height=Math.min(f[lo],f[hi]);
+		answer=Math.max(answer,height*2);
+		while(left<lo||hi<right) {
+			if(hi<right&&(lo==left||f[lo-1]<f[hi+1])) {
+				height=Math.min(f[++hi], height);
+			}else {
+				height=Math.min(f[--lo], height);
+			}
+			
+			answer= Math.max(answer, height*(hi-lo+1));
+		}
+		return answer;
+	}
+}
---
+++ KangNaeun/Algospot/DivideConquer/quadtree.java
@@ -0,0 +1,69 @@
+package DivideConquer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class quadtree {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		in.nextLine();
+		while(t-->0){
+			String s= in.nextLine();
+			StringBuffer sb= new StringBuffer(s);
+			System.out.println(reverse(sb,0));
+			
+			//iterator 사용
+			List<Character> listS=new ArrayList<Character>();
+			for(int i=0;i<s.length();i++) {
+				listS.add(s.charAt(i));
+			}
+			Iterator<Character> iterator= listS.iterator();
+			System.out.println(reverseIt(iterator));
+			
+		}
+		in.close();
+	}
+	
+	//Iterator 사용 함수
+	static String reverseIt(Iterator<Character> iterator) {
+		char head = iterator.next();
+		if(head!='x') return Character.toString(head);
+		
+		String upperLeft=reverseIt(iterator);
+		String upperRight=reverseIt(iterator);
+		String lowerLeft=reverseIt(iterator);
+		String lowerRight=reverseIt(iterator);
+		
+		return "x"+lowerLeft+lowerRight+upperLeft+upperRight;
+			
+	}
+
+	
+	static String reverse(StringBuffer s,int index) {
+		String reversed = s.substring(0,1);
+		if(s.length()==1) return reversed;
+		int start=++index;
+		int cut=0;
+		int cnt=0;
+		while(cnt++<4&&index<s.length()) {
+			if(cnt==3) cut=index;
+			if(s.charAt(index)=='x') {
+				String xs=reverse(s,index);
+				s.replace(index, index+xs.length(), xs);
+				index += xs.length()-1;
+			}
+			index++;
+		}
+		
+		reversed += s.substring(cut, index);
+		reversed += s.substring(start, cut);
+		
+		return reversed;
+	}
+	
+	
+}
