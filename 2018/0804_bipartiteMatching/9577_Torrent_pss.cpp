9ef106a64041ac4633ecff586b6a104e9ba137ca
pkiop <vivezdv@gmail.com>
2018-08-13 11:02:50 +0900

    .

--- 2018/0804_bipartiteMatching/9577_Torrent_pss.cpp
+++ 2018/0804_bipartiteMatching/9577_Torrent_pss.cpp
@@ -11,14 +11,17 @@ int n, m;
 int adj[200][200];
 int arr1[200], arr2[200];
 bool visited[200];
+
 bool dfs(int x) {
-	visited[x] = true;
-	for (int i = 1; i <= n; ++i) {
-		if (arr2[adj[x][i]] == -1 || !visited[arr2[adj[x][i]]] && dfs(arr2[adj[x][i]]))
-		{
-			arr1[x] = adj[x][i];
-			arr2[adj[x][i]] = x;
-			return true;
+	if (visited[x]) return false;
+	visited[x] = 1;
+	for (int j = 1; j <= n; ++j) {
+		if (adj[x][j]) {
+			if (arr2[j] == -1 || dfs(arr2[j])) {
+				arr1[x] = j;
+				arr2[j] = x;
+				return true;
+			}
 		}
 	}
 	return false;
@@ -28,6 +31,7 @@ int main(void) {
 	int t;
 	cin >> t;
 	while (t--) {
+		memset(adj, 0, sizeof(adj));
 		cin >> n >> m;
 		for (int i = 0; i < m; ++i) {
 			int t1, t2, num;
@@ -39,23 +43,25 @@ int main(void) {
 					adj[k][temp] = 1;
 				}
 			}
-			int size = 0;
-			memset(arr1, -1, sizeof(arr1));
-			memset(arr2, -1, sizeof(arr2));
-			for (int j = 0; j < 102; ++j) {
-				memset(visited, false, sizeof(visited));
-				if (dfs(j)) {
-					size++;
-					if (size == n) {
-						cout << j;
-						break;
-					}
-				}
-				if (j == 101) {
-					cout << -1;
+		}
+		int size = 0;
+		memset(arr1, -1, sizeof(arr1));
+		memset(arr2, -1, sizeof(arr2));
+		for (int j = 0; j < 102; ++j) {
+			memset(visited, false, sizeof(visited));
+			if (dfs(j)) {
+				size++;
+				if (size == n) {
+					cout << j;
+					break;
 				}
 			}
+			if (j == 101) {
+				cout << -1;
+			}
 		}
+
+		cout << endl;
 	}
 	return 0;
 }
