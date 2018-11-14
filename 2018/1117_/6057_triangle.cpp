b8e0cce1e3d41e8d2c5415475477b91cf30a9a0b
pkiop <vivezdv@gmail.com>
2018-11-14 22:08:59 +0900

    Update 2018/1117_/6057_triangle.cpp

--- 2018/1117_/6057_triangle.cpp
+++ 2018/1117_/6057_triangle.cpp
@@ -19,100 +19,86 @@ typedef long long ll;
 
 using namespace std;
 
-
-
 #define GO
 #define DEBUG
 
 
-/*
-c가 1개인데 다른게 2개면 fail 
 
+vector<int> arr[51]; // 연결리스트
+struct temp {
+	int a, b, c;
+};
+set<vector<int>> ans;
+bool visited[51];
+vector<int> temp(3);
 
-*/
-int arr[5]; // c r o a k 나온 횟수 count
+void go(int node, int start, int cnt,int first) {
+	visited[node] = true;
+	//cout << node << " cnt : " << cnt << endl;
+	int len = arr[node].size();
+	for (int i = 0; i < len; ++i) {
+		int next = arr[node][i];
+		if (cnt != 2 && node > next) {
+			continue;
+		}
+
+		if (cnt == 2 && next == start) {
+			//cout << "do " << endl;
+			temp[0] = start;
+			temp[1] = first;
+			temp[2] = node;
+				//	sort(temp.begin(), temp.end());
+//			for (int i = 0; i < 3; ++i) {
+	//			cout << temp[i] << ' ';
+	//		}
+	//		cout << endl;
+			ans.insert(temp);
+		}
+		else if(visited[next] == false && cnt != 2) {
+			//cout << "next : " << next << " start : " << start << endl;
+			if (cnt == 0) {
+				go(next, start, cnt + 1,first) ;
+			}
+			if (cnt == 1) {
+				go(next, start, cnt + 1, node);
+			}
+		}
+	}
+	visited[node] = false;
+}
 
 int main(void) {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #ifdef GO
 	freopen("input.txt", "r", stdin);
 #endif
+	int t;
+	scanf("%d", &t);
+	int ta, tb;
+	for(int step = 1;step <= t; ++step){
+		int n, m;
+		scanf("%d %d", &n, &m);
+		for (int i = 0; i < m; ++i) {
+			scanf("%d %d", &ta, &tb);
+			arr[tb].push_back(ta);
+			arr[ta].push_back(tb);
+		}
 
-	int n;
-	cin >> n;
-	for(int test = 1;test <= n ; ++test){
-		string s;
-		memset(arr, 0, sizeof(arr));
-		cin >> s;
-		int ret = 0;
-		int len = s.size();
-		for (int i = 0; i < len; ++i) {
-			if (s[i] == 'c') {
-				arr[0]++;
-			}
-			if (s[i] == 'r') {
-				if (arr[1] >= arr[0])
-					ret = 9999;
-				arr[1]++;
-			}
-			if (s[i] == 'o') {
-				for (int j = 0; j < 2; ++j)
-					if (arr[2] >= arr[j])
-						ret = 9999;
-				arr[2]++;
-			}
-			if (s[i] == 'a') {
-				for (int j = 0; j < 3; ++j)
-					if (arr[3] >= arr[j])
-						ret = 9999;
-				arr[3]++;
-			}
-			if (ret == 9999) {
-				break;
-			}
-			if (s[i] == 'k') {
-
-				for (int j = 0; j < 4; ++j) {
-					if (arr[j] == 0) {
-						ret = 9999;
-						break;
-					}
-					ret = max(arr[j], ret);
-					arr[j]--;
-				}
-			}
-			
+		//연결리스트 저장 끝
+	
+		//돌자
+		for (int i = 1; i <= n; ++i) {
+			//cout << "start : " << i << endl;
+			memset(visited, false, sizeof(visited));
+			go(i, i, 0, -1);
 		}
-		for (int i = 0; i < 4; ++i) {
-			if (arr[i] != 0) {
-				ret = 9999;
-				break;
-			}
-		}
-		cout << '#' << test << ' ';
-		if (ret == 9999) {
-			cout << -1 << endl;
-		}
-		else {
-			cout << ret << endl;
+		printf("#%d %d\n", step, ans.size());
+		ans.clear();
+		for (int i = 0; i <= n; ++i) {
+			arr[i].clear();
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 #ifdef GO
 	cout << "\nerase go\n";
 #endif
